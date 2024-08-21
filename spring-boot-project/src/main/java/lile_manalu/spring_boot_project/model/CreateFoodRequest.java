7ede7141de17c72f5b8d17f82ef1b62a7d8126dd
Lile Manalu <lilemanalu207@gmail.com>
2024-08-21 20:15:57 +0700

    Update model for create food API (request)

--- spring-boot-project/src/main/java/lile_manalu/spring_boot_project/model/CreateFoodRequest.java
+++ spring-boot-project/src/main/java/lile_manalu/spring_boot_project/model/CreateFoodRequest.java
@@ -1,10 +1,13 @@
 package lile_manalu.spring_boot_project.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lile_manalu.spring_boot_project.entity.AddOn;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -20,4 +23,6 @@ public class CreateFoodRequest {
     private String description;
 
     private Double price;
+
+    private List<AddOn> addOns;
 }

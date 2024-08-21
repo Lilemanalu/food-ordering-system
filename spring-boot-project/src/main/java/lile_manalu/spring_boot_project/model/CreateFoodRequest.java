90f345ecbbf66a95adf73c5841f356e02230499f
Lile Manalu <lilemanalu207@gmail.com>
2024-08-21 16:54:42 +0700

    Create model for CreateFoodRequest

---
+++ spring-boot-project/src/main/java/lile_manalu/spring_boot_project/model/CreateFoodRequest.java
@@ -0,0 +1,23 @@
+package lile_manalu.spring_boot_project.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateFoodRequest {
+
+    @JsonIgnore
+    private String outlet_id;
+
+    private String name;
+
+    private String description;
+
+    private String price;
+}

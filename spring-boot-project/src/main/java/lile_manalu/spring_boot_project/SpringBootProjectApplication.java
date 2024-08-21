727c39fb44a13862d61cb6a330bd5d8b4fb1baa9
Lile Manalu <lilemanalu207@gmail.com>
2024-08-21 11:05:23 +0700

    Initialize project

---
+++ spring-boot-project/src/main/java/lile_manalu/spring_boot_project/SpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package lile_manalu.spring_boot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ spring-boot-project/src/test/java/lile_manalu/spring_boot_project/SpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lile_manalu.spring_boot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

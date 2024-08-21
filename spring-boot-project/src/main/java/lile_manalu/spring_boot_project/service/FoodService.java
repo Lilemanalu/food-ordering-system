0ba574f002a366e30b032bd5f00f00cb1066f7bf
Lile Manalu <lilemanalu207@gmail.com>
2024-08-21 20:35:11 +0700

    Implement get all food API in FoodService.java

--- spring-boot-project/src/main/java/lile_manalu/spring_boot_project/service/FoodService.java
+++ spring-boot-project/src/main/java/lile_manalu/spring_boot_project/service/FoodService.java
@@ -14,7 +14,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -84,6 +86,16 @@ public class FoodService {
             logger.error("Error deleting food item with ID: {}. Error: {}", foodId, e.getReason());
             throw e;
         }
+    }
+
+    public List<FoodResponse> list() {
+        List<Food> foods = foodRepository.findAll();
+        List<FoodResponse> foodResponses = foods.stream()
+                .map(this::toFoodResponse)
+                .collect(Collectors.toList());
+
+        logger.info("Fetched {} food items", foodResponses.size());
+        return foodResponses;
     }
 
     private FoodResponse toFoodResponse(Food food){

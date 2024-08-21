01155f853ed2e5606b7597b9851b19050b67af25
Lile Manalu <lilemanalu207@gmail.com>
2024-08-21 20:16:06 +0700

    Update model for create food API (response)

---
+++ spring-boot-project/src/main/java/lile_manalu/spring_boot_project/model/CreateFoodResponse.java
@@ -0,0 +1,29 @@
+package lile_manalu.spring_boot_project.model;
+
+import lile_manalu.spring_boot_project.entity.AddOn;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateFoodResponse {
+
+    private String id;
+
+    private String outlet_id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    private List<AddOn> addOns;
+
+}

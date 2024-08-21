170479f63eb2a2b764192c1e371f37217d29394c
Lile Manalu <lilemanalu207@gmail.com>
2024-08-21 16:41:25 +0700

    Create WebResponse.java

---
+++ spring-boot-project/src/main/java/lile_manalu/spring_boot_project/model/WebResponse.java
@@ -0,0 +1,17 @@
+package lile_manalu.spring_boot_project.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WebResponse<T> {
+
+    private T data;
+
+    private String errors;
+}

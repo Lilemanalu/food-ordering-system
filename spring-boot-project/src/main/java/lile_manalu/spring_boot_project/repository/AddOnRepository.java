53af462a0d8ebbc22b9fb8cfaf6be607c95f2f9e
Lile Manalu <lilemanalu207@gmail.com>
2024-08-22 09:11:57 +0700

    Update AddOnRepository.java

--- spring-boot-project/src/main/java/lile_manalu/spring_boot_project/repository/AddOnRepository.java
+++ spring-boot-project/src/main/java/lile_manalu/spring_boot_project/repository/AddOnRepository.java
@@ -8,8 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface AddOnRepository extends JpaRepository<AddOn, String> {
-    @Override
-    Optional<AddOn> findById(String id);
-
-    Optional<AddOn> findFirstByFoodIdAndId(String food_id, String id);
+    Optional<AddOn> findFirstByFoodIdAndId(String foodId, String id);
 }

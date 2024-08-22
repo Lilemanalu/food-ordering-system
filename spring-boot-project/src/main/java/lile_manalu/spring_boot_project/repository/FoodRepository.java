bceff232102625b3a79ba1fb9a137ef609b0aa55
Lile Manalu <lilemanalu207@gmail.com>
2024-08-22 09:47:23 +0700

    Update FoodRepository.java

--- spring-boot-project/src/main/java/lile_manalu/spring_boot_project/repository/FoodRepository.java
+++ spring-boot-project/src/main/java/lile_manalu/spring_boot_project/repository/FoodRepository.java
@@ -4,10 +4,13 @@ import lile_manalu.spring_boot_project.entity.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FoodRepository extends JpaRepository<Food, String> {
     @Override
     Optional<Food> findById(String id);
+
+    List<Food> findByOutletId(String outletId);
 }

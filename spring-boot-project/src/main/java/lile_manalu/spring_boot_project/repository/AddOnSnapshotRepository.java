2585ca4cdc9598da68478987cf6530a0b556c287
Lile Manalu <lilemanalu207@gmail.com>
2024-08-22 12:37:38 +0700

    Update AddOnSnapshotRepository

--- spring-boot-project/src/main/java/lile_manalu/spring_boot_project/repository/AddOnSnapshotRepository.java
+++ spring-boot-project/src/main/java/lile_manalu/spring_boot_project/repository/AddOnSnapshotRepository.java
@@ -4,10 +4,13 @@ import lile_manalu.spring_boot_project.entity.AddOnSnapshot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AddOnSnapshotRepository extends JpaRepository<AddOnSnapshot, String> {
     @Override
     Optional<AddOnSnapshot> findById(String id);
+
+    List<AddOnSnapshot> findByFoodSnapshotId(String snapshotId);
 }

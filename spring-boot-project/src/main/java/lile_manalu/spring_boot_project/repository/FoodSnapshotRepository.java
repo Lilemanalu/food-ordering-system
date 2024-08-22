4d07afab582967950b57a1db19949a924e4eb28f
Lile Manalu <lilemanalu207@gmail.com>
2024-08-22 12:37:07 +0700

    Update FoodSnapshotRepository

--- spring-boot-project/src/main/java/lile_manalu/spring_boot_project/repository/FoodSnapshotRepository.java
+++ spring-boot-project/src/main/java/lile_manalu/spring_boot_project/repository/FoodSnapshotRepository.java
@@ -4,10 +4,13 @@ import lile_manalu.spring_boot_project.entity.FoodSnapshot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FoodSnapshotRepository extends JpaRepository<FoodSnapshot, String> {
     @Override
     Optional<FoodSnapshot> findById(String id);
+
+    List<FoodSnapshot> findByOrderId(String orderId);
 }
